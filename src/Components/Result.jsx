cd36048cd06d6cd2cae3f56a3fb0650c15eb020d
Varun Kodamanchili <varun132413@gmail.com>
2025-01-15 10:17:07 +0530

    router changes

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,17 +1,18 @@
 import React ,{useState,useEffect}from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation,useNavigate } from 'react-router-dom';
 import {Icon} from 'semantic-ui-react';
 const Result = () => {
     const location = useLocation();
     const { score, answers } = location.state || { score: 0, answers: {} };
     const [icon,setIcon]=useState(false);
+    const navigate = useNavigate();
     useEffect(()=>{
         if(score>0){
             setIcon(true);
         }
     },[score])
     setTimeout(()=>{
-        window.location.href='/subject';
+        navigate('/subject');
     },5000)
     return (
         <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',height:'100vh'}}>
@@ -20,6 +21,7 @@ const Result = () => {
                 icon?<Icon name='check circle' color='green' size='massive' />:<Icon name='times circle' color='red' size='massive' />
             }
             <h3>Your Score : {score}</h3>
+            <span>Redirecting to Dashboard....</span>
         </div>
     );
 };
